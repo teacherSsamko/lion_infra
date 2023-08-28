7079e70f9c0d52c0005c9518d30798318ffd3c03
Eunsub Lee <ssamko@kakao.com>
2023-08-28 14:49:33 +0900

    FEAT production

---
+++ production/variables.tf
@@ -0,0 +1,15 @@
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+variable "password" {
+  type = string
+}
+
+variable "db_password" {
+  type = string
+}
